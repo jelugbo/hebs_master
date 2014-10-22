736e188b39eeab53fe090396bc80fc66b5250d46
jelugbo <jelugbo@gmail.com>
2014-10-22 11:15:00 -0500

    paypal

--- lms/djangoapps/shoppingcart/processors/PayPal.py
+++ lms/djangoapps/shoppingcart/processors/PayPal.py
@@ -57,8 +57,8 @@ def start_payment_process(params):
         'PWD': settings.CC_PROCESSOR['PayPal'].get('USER_PWD', ''),
         'SIGNATURE': settings.CC_PROCESSOR['PayPal'].get('USER_SIGN', ''),
         'VERSION': settings.CC_PROCESSOR['PayPal'].get('API_VERSION', '78'),
-        'METHOD': 'DoDirectPayment'
-        'PAYMENTACTION': 'SALE'
+        'METHOD': 'DoDirectPayment',
+        'PAYMENTACTION': 'SALE',
         'AMT' : int(order.total_cost * 100),
         'AMOUNT': int(order.total_cost * 100),
         'CURRENCY': order.currency.upper(),
